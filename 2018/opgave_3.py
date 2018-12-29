6b9b79195897c8181035d400174ce7bc509460a0
Wouter Theune <>
2018-12-29 14:57:26 +0100

    added opgace_3

---
+++ 2018/opgave_3.py
@@ -0,0 +1,11 @@
+OPGAVE_PRE = "KERST"
+OPGAVE_POST_1 = "REKENEN"
+OPGAVE_POST_2 = "MET"
+OPAGVE_POST_3 = "TIEN"
+OPGAVE_POST_4 = "LETTERS"
+
+VALUES = [0,1,2,3,4,5,6,7,8,9]
+CHARS = ["K", "E", "R", "S", "T", "N", "M", "I", "L"]
+
+def calculate():
+    for char in CHARS:
